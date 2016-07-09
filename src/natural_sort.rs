95af2fbcd1f116d32ca5c346a1a71d1ab78ca375
stereobooster <stereobooster@gmail.com>
2016-07-10 01:15:01 +0300

    Temp commit

--- src/natural_sort.rs
+++ src/natural_sort.rs
@@ -6,17 +6,28 @@ extern crate postgres_extension;
 #[macro_use]
 extern crate postgres_extension_macros;
 
-extern crate libc;
+use postgres_extension::{VarChar};
 
+extern crate libc;
 use libc::{c_int};
+
+use std::str::FromStr;
+use std::cmp::Ordering;
+extern crate natord;
 
 pg_module!(version: 90500);
 
 #[pg_export]
-pub fn natural_sort(a: c_int) -> c_int {
-  if a == 0 {
-    return 1
+pub fn naturel_compare(a: VarChar, b: VarChar) -> c_int {
+  let a_str = a.p;
+  let b_str = b.p;
+  let res = natord::compare(a_str, b_str);
+
+  if res == Ordering::Greater {
+    return 1;
+  } else if res == Ordering::Less {
+    return -1;
   } else {
-    return 0
+    return 0;
   }
 }
